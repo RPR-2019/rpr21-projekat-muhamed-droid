a8cec45182d3ab929aa63dde652883a867efbb1f
muhamed-droid <mmasnopita1@etf.unsa.ba>
2022-02-15 12:07:56 +0100

    Prvi commit, nema nista urađeno

---
+++ src/ba/unsa/etf/rpr/Osoba.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Date;
+
+public class Osoba {
+    private String ime;
+    private String prezime;
+    private String adresa;
+    private Date datum_rodjenja;
+    private String država_rodjenja;
+    private String JMBG;
+
+
+
+}
